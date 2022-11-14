f7da9da957f2585faa5f6d2404b9486c1cf27240
Masuvat <prof.masuvat@gmail.com>
2022-11-15 01:46:28 +0545

    Stable Build Established

---
+++ components/WorkExperience.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {motion} from "framer-motion"
+import ExperienceCard from '../components/ExperienceCard'
+import { Experience } from '../typings'
+
+type Props = {
+  experiences: Experience[];
+}
+
+export default function WorkExperience({experiences}: Props) {
+  return (
+    <motion.div 
+    initial={{x:-200,opacity:0}}
+    transition={{duration:1.2,}}
+    whileInView={{x:0,opacity:1}}
+
+    className="flex flex-col relative h-screen text-center md:text-left
+    md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+        <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+            Work Experience
+        </h3>
+        <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory
+        scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
+            {experiences?.map((experience)=>(
+              <ExperienceCard key={experience._id} experience={experience}/>
+            ))}
+        </div>
+    </motion.div>
+  )
+}
